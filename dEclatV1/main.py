b8b68601310c25c1ec571b74d1fae785dee7dc45
Maja TZ <majatureczekzakrzewska@gmail.com>
2021-12-12 07:48:04 +0100

    BTC test

--- dEclatV1/main.py
+++ dEclatV1/main.py
@@ -9,12 +9,13 @@ def generate_data():
 #get_200_tweets("elonmusk")
 #get_200_tweets("Meta")
 #get_200_tweets("troyhunt")
-
-words = ["covid", "vaccine", "covid-19", "quarantine", "restrictions", "phizer", "moderna", "astrazeneca",
-"fake covid", "wuhan", "coronavirus", "health", "pandemic", "virus", "corona", "lockdown",
-"unvaccinated", "omicron", "sars-cov-2", "death", "antibodies", "plandemic","government idiots","fear","happiness"]
+words = ["bitcoin", "insecure", "crypto", "inflation", "ETH","billionaire","money", "India", "secure", "price","legal","tender","future","official", "death"]
+search_tweets_by_tags(words,"BTC")
+#words = ["covid", "vaccine", "covid-19", "quarantine", "restrictions", "phizer", "moderna", "astrazeneca",
+#"fake covid", "wuhan", "coronavirus", "health", "pandemic", "virus", "corona", "lockdown",
+#"unvaccinated", "omicron", "sars-cov-2", "death", "antibodies", "plandemic","government idiots","fear","happiness"]
     #
-search_tweets_by_tags(words, "covid")
+#search_tweets_by_tags(words, "covid")
 
 #words = ["covid fake plandemic", "covid-19 planned government", "fake covid sard-cov-2 pandemic", "covid-19 safe propaganda",
 #"sars covid", "covid flatten earth", "virus omicron holidays"]
@@ -47,13 +48,13 @@ if __name__ == "__main__":
 
     generate_data()
 
-    memory, exec_time = main(data_path=r"data/db-covid.txt",
+    memory, exec_time = main(data_path=r"data/db-BTC.txt",
                              min_supp=20,
                              show_supp=True,
                              show_one_elem_fi=True,
                              save_fis=True,
-                             out_name="output-covidtry1",
+                             out_name="output-btc1",
                              spmf_file=True,
-                             spmf_name="spmf-transactions-covid")
+                             spmf_name="spmf-transactions-btc")
 
     print(f"Maximum memory usage: {memory * 0.000001} Megabytes.\nTotal time ~ {exec_time} seconds.")
